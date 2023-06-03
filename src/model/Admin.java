427023ee31c2690c752a272516b378ba34138bab
SrinidhiBoopathi <107973972+SrinidhiBoopathi@users.noreply.github.com>
2023-06-03 21:19:59 +0530

    Create Admin.java

---
+++ src/model/Admin.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+import service.Authentication;
+
+import java.util.*;
+/**
+ *
+ * @author srini
+ */
+public class Admin extends Person{
+
+    String adminId;
+  public static  String username="admin";
+   public static String password="123";
+    
+    
+}
